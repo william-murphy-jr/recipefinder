e590ead80aaf290dc2bc9fb546008a28979bbd82
Bill Murphy <bill559m@gmail.com>
2020-02-15 18:37:35 -0800

    added blank config file

---
+++ config/config-sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  database: {
+    user: '****user ****',
+    password: '**** password ****',
+    databaseName: '**** db name ****'
+  },
+  apiKey1: 'PLACE KEY HERE',
+  apiKey2: 'PLACE KEY HERE'
+};
--- src/components/SearchRecipes.jsx
+++ src/components/SearchRecipes.jsx
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: {},
+    }
+
+    const options = {
+      method: 'GET',
+      uri: `https://api.themoviedb.org/3/search/movie?api_key=${config.api_key1}&query=${text}`,
+      headers: {
+        'User-Agent': 'Mozilla'
+      },
+      json: true
+    };
+    
+
+
+  }
+
   render() {
     return (
       <Form inline>
@@ -22,4 +41,4 @@ class SearchRecipes extends Component {
   }
 }
 
-export default SearchRecipes;
+export default SearchRecipes; 
